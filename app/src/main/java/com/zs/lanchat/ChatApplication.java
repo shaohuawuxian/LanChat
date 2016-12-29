c6b3b2d13a2e519b324156db23e5eea7d1fdb084
unknown <zhangshao@A06P-A9185543.dangdang.com>
2016-12-29 15:18:15 +0800

    first commit

---
+++ app/src/main/java/com/zs/lanchat/ChatApplication.java
@@ -0,0 +1,10 @@
+package com.zs.lanchat;
+
+import android.app.Application;
+
+/**
+ * Created by zhangshao on 2016/12/29.
+ */
+
+public class ChatApplication extends Application {
+}
---
+++ app/src/main/java/com/zs/lanchat/activities/WelcomeActivity.java
@@ -0,0 +1,10 @@
+package com.zs.lanchat.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zhangshao on 2016/12/29.
+ */
+
+public class WelcomeActivity extends AppCompatActivity {
+}
